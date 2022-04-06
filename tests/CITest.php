aad80c314d88640c8dd4110c76d15bcf612e12c1
amiribenabdellah.personal <amiribenabdellah@gmail.com>
2022-04-06 07:59:05 +0200

    test

--- tests/CITest.php
+++ tests/CITest.php
@@ -6,7 +6,7 @@ class CITest extends TestCase
     public function testCI(){
         $swagTest = new SwagTest();
         $val = $swagTest->testCI();
-        static::assertEquals(42, $val);
+        static::assertEquals(42, $val)
     }
 
 }
